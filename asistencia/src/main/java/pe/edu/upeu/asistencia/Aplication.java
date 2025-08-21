5a10f97c9b20656b616a32cae250083d4c0da1c7
diegor100000-git <diegor100000@gmail.com>
2025-08-21 10:27:20 -0500

    sdsd

---
+++ asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -0,0 +1,10 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World : JCLV");
+        AsistenciaApplication.main(args);
+    }
+}
